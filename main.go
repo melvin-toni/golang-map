3a5e843c6abb1a81fa934ee255721e8c0d55f37a
Melvin Toni <melvin.working@gmail.com>
2021-11-16 11:59:10 +0700

    manipulating map

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	/*
+		Another way of declaring map
+
+		var colors map[string]string
+
+		it will produce GO ZERO VALUE which is map[]
+	*/
+
+	/*
+		Another way of declaring map
+
+		colors := map([string]string)
+	*/
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#0ff000",
 	}
+
+	/* add value into existing map */
+	colors["white"] = "#ffffff"
+
+	/* delete value in map */
+	delete(colors, "green")
 
 	fmt.Println(colors)
 }
